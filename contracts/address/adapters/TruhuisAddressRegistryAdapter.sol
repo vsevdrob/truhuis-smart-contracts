56ea8caa5c55a8a9c80bb3978f914d71cbdc5e2f
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-06-11 22:19:27 +0200

    Included NatSpec annotations

--- contracts/address/adapters/TruhuisAddressRegistryAdapter.sol
+++ contracts/address/adapters/TruhuisAddressRegistryAdapter.sol
@@ -11,42 +11,67 @@ import "../../../interfaces/ITruhuisCurrencyRegistry.sol";
 import "../../../interfaces/ITruhuisCadastre.sol";
 import "../../../interfaces/ITruhuisMarketplace.sol";
 
+/// @title Truhuis AddressRegistry Adapter abstract smart contract.
+/// @notice This contract deployed in order to be able to perform the necessary
+///         smart contract function calls in the most convenient way.
 abstract contract TruhuisAddressRegistryAdapter is Ownable {
+    /// @notice Reverted if an address registry update fails.
+    error TruhuisAddressRegistryAdapter__AddressRegistryUpdateFailed();
+
+    /// @notice Truhuis Address Registry smart contract address.
     ITruhuisAddressRegistry private _addressRegistry;
 
-    function updateAddressRegistry(address _registry) public virtual onlyOwner {
-        _updateAddressRegistry(_registry);
+    /// @notice Event emitted when an address registry update occurs.
+    event AddressRegistryUpdated(
+        address contractAddr
+    );
+
+    /// @notice Update Truhuis Address Registry smart contract address.
+    /// @param _registry The new Truhuis Address Registry smart contract address.
+    /// @dev Only owner is able to call this function.
+    function updateAddressRegistry(address _registry) public onlyOwner {
+        _addressRegistry = ITruhuisAddressRegistry(_registry);
     }
 
-    function auction() public view virtual returns (ITruhuisAuction) {
-        return ITruhuisAuction(_addressRegistry.auction());
-    }
-
-    function addressRegistry() public view virtual returns (ITruhuisAddressRegistry) {
+    /// @notice Returns Truhuis Address Registry smart contract interface.
+    function addressRegistry() public view returns (ITruhuisAddressRegistry) {
         return _addressRegistry;
     }
 
-    function citizen(address _citizen) public view virtual returns (ICitizen) {
+    /// @notice Returns Truhuis Auction smart contract interface.
+    function auction() public view returns (ITruhuisAuction) {
+        return ITruhuisAuction(_addressRegistry.auction());
+    }
+
+    /// @notice Returns Citizen smart contract interface specified by
+    ///         the Citizen smart contract address.
+    ///
+    /// @param _citizen A Citizen smart contract address.
+    function citizen(address _citizen) public view returns (ICitizen) {
         return ICitizen(_citizen);
     }
 
-    function currencyRegistry() public view virtual returns (ITruhuisCurrencyRegistry) {
+    /// @notice Returns Truhuis Currency Registry smart contract interface.
+    function currencyRegistry() public view returns (ITruhuisCurrencyRegistry) {
         return ITruhuisCurrencyRegistry(_addressRegistry.currencyRegistry());
     }
 
-    function stateGovernment(bytes3 _country) public view virtual returns (IStateGovernment) {
+    /// @notice Returns State Government smart contract interface specified by
+    ///         the State Government smart contract address.
+    ///
+    /// @param _country Country in the form of ISO 3166-1 Alpha-3 code (e.g. "NLD" or "BEL")
+    ///        to which the state government belongs.
+    function stateGovernment(bytes3 _country) public view returns (IStateGovernment) {
         return IStateGovernment(_addressRegistry.stateGovernment(_country));
     }
 
-    function cadastre() public view virtual returns (ITruhuisCadastre) {
+    /// @notice Returns Truhuis Cadastre smart contract interface.
+    function cadastre() public view returns (ITruhuisCadastre) {
         return ITruhuisCadastre(_addressRegistry.cadastre());
     }
 
-    function marketplace() public view virtual returns (ITruhuisMarketplace) {
+    /// @notice Returns Truhuis Marketplace smart contract interface.
+    function marketplace() public view returns (ITruhuisMarketplace) {
         return ITruhuisMarketplace(_addressRegistry.marketplace());
-    }
-
-    function _updateAddressRegistry(address _registry) internal virtual {
-        _addressRegistry = ITruhuisAddressRegistry(_registry);
     }
 }
