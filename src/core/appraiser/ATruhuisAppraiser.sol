c7116fb269537737aa7b6dba1d02454f7d368a30
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-07-07 17:45:26 +0200

    feat: make appraiser ownable

--- src/core/appraiser/ATruhuisAppraiser.sol
+++ src/core/appraiser/ATruhuisAppraiser.sol
@@ -3,8 +3,9 @@
 pragma solidity 0.8.13;
 
 import "../address/TruhuisAddressRegistryAdapter.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract ATruhuisAppraiser is TruhuisAddressRegistryAdapter {
+abstract contract ATruhuisAppraiser is Ownable, TruhuisAddressRegistryAdapter {
     constructor(address _addressRegistry) {
         updateAddressRegistry(_addressRegistry);
     }
