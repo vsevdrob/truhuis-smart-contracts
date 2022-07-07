8ab254342a815f473603830b8d3f804b7ea2cf99
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-07-07 14:17:26 +0200

    test: initialized unit tests

---
+++ test/appraiser/TruhuisAppraiser.t.sol
@@ -0,0 +1,13 @@
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import "@test/Conftest.sol";
+
+/**
+ * @title TruhuisAppraiser
+ * @author vsevdrob
+ * @notice _
+ */
+contract TruhuisAddressRegistryTest is Conftest {}
+
