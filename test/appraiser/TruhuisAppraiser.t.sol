a980a56b1bc9d73e7fb8e8984381387629c567aa
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-07-07 15:36:06 +0200

    test: wrote unit test functions

--- test/appraiser/TruhuisAppraiser.t.sol
+++ test/appraiser/TruhuisAppraiser.t.sol
@@ -5,9 +5,31 @@ pragma solidity 0.8.13;
 import "@test/Conftest.sol";
 
 /**
- * @title TruhuisAppraiser
+ * @title TruhuisAppraiserTest
  * @author vsevdrob
- * @notice _
+ * @dev List of implemented functions to test (PASS | FAIL | TODO):
+ *      [PASS] constructor()
  */
-contract TruhuisAddressRegistryTest is Conftest {}
+contract TruhuisAppraiserTest is Conftest {
+    function setUp() public {
+        _deploy();
+    }
+
+    function testConstructor() external {
+        /* ARRANGE */
+
+        // Deploy a new TruhuisAppraiser contract.
+        appraiser = new TruhuisAppraiser(address(addressRegistry));
+
+        /* ACT */
+        
+        // Get address registry contract address.
+        address addressRegistryAddr = address(appraiser.addressRegistry());
+
+        /* PERFORM ASSERTIONS */
+
+        // Actual contract address must be equal to the expected.
+        assertEq(address(addressRegistry), addressRegistryAddr);
+    }
+}
 
