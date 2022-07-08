f4fc1236a49cbc31e749272c142045ef33534ad8
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-07-08 13:39:53 +0200

    test: initialized unit test contract

---
+++ test/cadastre/TruhuisCadastre.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import "@test/Conftest.sol";
+
+/**
+ * @title TruhuisCadastreTest
+ * @author vsevdrob
+ * @dev List of implemented functions to test (PASS | FAIL | TODO):
+ *      [TODO] constructor(address,string)
+ *      [TODO] allotTokenURI(string,uint256)
+ *      [TODO] confirmTransfer(uint256,uint256)
+ *      [TODO] exists(uint256)
+ *      [TODO] isNFTOwner(uint256)
+ *      [TODO] pause()
+ *      [TODO] produceNFT(address,string)
+ *      [TODO] safeTransferFrom(address,address,uint256)
+ *      [TODO] safeTransferFrom(address,address,uint256,bytes)
+ *      [TODO] submitTransfer(uint256,uint256)
+ *      [TODO] supportsInterface(bytes4)
+ *      [TODO] revokeTransferConfirmation(uint256,uint256)
+ *      [TODO] tokenURI(uint256)
+ *      [TODO] transferFrom(address,address,uint256)
+ *      [TODO] transferNFTOwnership(address,address,bytes,uint256,uint256)
+ *      [TODO] unpause()
+ *      [TODO] updateContractURI(string)
+ */
+contract TruhuisCadastreTest is Conftest {}
