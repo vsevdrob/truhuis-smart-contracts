c2b27c03b646a9d839e1c8bfcca54d928f59c169
moonwake769 <exhilaratedreverie288@protonmail.ch>
2022-08-05 23:29:52 +0200

    docs: added error and func definitions

--- src/interfaces/ICurrencyRegistry.sol
+++ src/interfaces/ICurrencyRegistry.sol
@@ -2,7 +2,9 @@
 
 pragma solidity 0.8.13;
 
+// Reverted if provided token address has been already registered.
 error CURRENCY_ALREADY_REGISTERED();
+// Reverted if provided token address is not registered.
 error CURRENCY_NOT_REGISTERED();
 
 interface ICurrencyRegistry {
@@ -12,22 +14,23 @@ interface ICurrencyRegistry {
     event CurrencyUnregistered(address tokenAddr);
 
     /**
-     * @dev _
+     * @dev Register new currency in the form of an ERC-20 token address.
      */
     function registerCurrency(address _tokenAddr) external;
 
     /**
-     * @dev _
+     * @dev Unregister currency.
      */
     function unregisterCurrency(address _tokenAddr) external;
 
     /**
-     * @dev _
+     * @dev Get whether currency is allowed to be used as payment currency or
+     *      not.
      */
     function isAllowedCurrency(address _token) external view returns (bool);
 
     /**
-     * @dev _
+     * @dev Get whether currency has been registered or not.
      */
     function isRegisteredCurrency(address _token) external view returns (bool);
 }
